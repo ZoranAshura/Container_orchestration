d6e36f1aa59c1cdd55c2175f19fd7b4a05d03eee
Timur Bektemirov <tima.bektemirov.41@mail.ru>
2024-04-14 09:08:02 +0300

    part-3

---
+++ src/vagrant/docker-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update 
+sudo apt install -y docker.io 
+sleep 15
+echo "just something"
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y 
---
+++ src/vagrant/docker-swarm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start docker service
+sudo systemctl start docker
+sudo apt-get install sshpass
+
+# Creating a Swarm by initializing a manager node
+ipAddress=$(ip addr | grep "eth1" | awk 'NR==2' | awk '{print $2}' | awk -F "/" '{print $1}')
+token=$(sudo docker swarm init --advertise-addr ${ipAddress} | awk "NR==5" | awk '{ for (i=1; i<=NF; i++) printf "%s ", $i; printf "\n" }')
+
+# Saving the command
+echo "sudo $token" > workers.sh
+chmod 777 workers.sh
+
+# Adding worker nodes to the Swarm
+sshpass -p "vagrant" ssh vagrant@172.16.1.50 'sudo systemctl start docker'
+sshpass -p "vagrant" scp workers.sh vagrant@172.16.1.50:/home/vagrant/
+sshpass -p "vagrant" ssh vagrant@172.16.1.50 '/home/vagrant/workers.sh'
+sshpass -p "vagrant" ssh vagrant@172.16.1.51 'sudo systemctl start docker'
+sshpass -p "vagrant" scp workers.sh vagrant@172.16.1.51:/home/vagrant/
+sshpass -p "vagrant" ssh vagrant@172.16.1.51 '/home/vagrant/workers.sh'
